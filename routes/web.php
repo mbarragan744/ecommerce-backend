a54a00dd578982f967fdc838eefcfc1813b0d7c8
Mariana Barragan <mbarragan744@unab.edu.co>
2025-02-25 15:22:12 -0500

    Rutas Basico

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    echo "LISTADO DE PRODUCTOS DEL ECOMMERCE";
+});
+
+Route::get('/products/create', function () {
+    echo "FORMULARIO PARA CREAR UN PRODUCTO";
+});
+
+Route::get('/products/{name}', function ($name) {
+    echo "EL PRODUCTO ES: $name";
+});
+
+Route::get('/products/{name}/{categoria?}', function ($name,$categoria=null) {
+    if ($categoria=null){
+        echo "EL PRODUCTO ES: $name";
+    }
+    else{
+        echo "El producto es: $name, y su categoria es: $categoria";
+    }
+    
+});
