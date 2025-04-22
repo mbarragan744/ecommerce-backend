1f080dd1ae3c6ded87000d019343631abb97cc6c
mbarragan744 <mbarragan744@unab.edu.co>
2025-04-22 14:03:45 -0500

    Ajuste para preparar el proyecto para laravel ui auty

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,32 +6,7 @@
     <title>@yield('title')</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        .navbar {
-            background-color: #fc9928; /* Naranja UNAB */
-        }
-        .navbar a {
-            color: white !important;
-        }
-        .navbar a:hover {
-            color: #6A4C9C; 
-        }
-        .btn-primary {
-            background-color: #6A4C9C; 
-            border-color: #6A4C9C;
-        }
-        .btn-primary:hover {
-            background-color: #5c43a0;
-            border-color: #5c43a0;
-        }
-        footer {
-            background-color: #fc9928;
-            color: white;
-        }
-        footer a {
-            color: white;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 </head>
 <body>
 
